100ab1109dc6547fdbd8a54e477b16e39d404d7a
MeyerZhao <zhao_min_yuan@126.com>
2017-07-06 15:28:24 +0800

    路由修改

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Router, Route, hashHistory } from 'react-router'
 import App from './App';
-import Users from './routes/Users';
+import User from './routes/user/';
 
 
 ReactDOM.render((
 	<Router history={hashHistory}>
 	    <Route path="/" component={App}>
-		    <Route path="/users" component={Users} />
+		    <Route path="/user" component={User} />
 	    </Route>
 	  </Router>
 ), document.getElementById('root'));
