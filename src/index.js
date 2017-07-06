659cb158b9b789d43e6368263fffe1e7ae1cf118
MeyerZhao <zhao_min_yuan@126.com>
2017-07-06 17:03:19 +0800

    美化路径 去掉路径中的#

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, browserHistory } from 'react-router'
 import App from './App';
-import User from './routes/user/';
+import UserList from './routes/user/userlist/';
+import BusinessType from './routes/user/businesstype/';
+import BusinessList from './routes/user/businesslist/';
 
 
 ReactDOM.render((
-	<Router history={hashHistory}>
+	<Router history={browserHistory}>
 	    <Route path="/" component={App}>
-		    <Route path="/user" component={User} />
+	    	<Route path="user">
+			    <Route path="userlist" component={UserList} />
+			    <Route path="businesstype" component={BusinessType} />
+			    <Route path="businesslist" component={BusinessList} />
+	    	</Route>
 	    </Route>
 	  </Router>
 ), document.getElementById('root'));
