6baed0b0aa4f66573dbfbb07e81359d66835309f
MeyerZhao <zhao_min_yuan@126.com>
2017-07-07 14:58:25 +0800

    写好路由 面包屑导航
    面包屑导航 略有问题 并不是浏览器段的

--- src/App.js
+++ src/App.js
@@ -1,89 +1,138 @@
-import React, { Component } from 'react';
+import React from 'react'
 import './App.css';
-import { Link } from 'react-router'
+import { Link } from 'react-router';
 import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 const { Header, Content, Footer, Sider } = Layout;
 const SubMenu = Menu.SubMenu;
 
-
-
-
-class App extends Component {
+class App extends React.Component {
 	state = {
-    collapsed: false,
-    mode: 'inline',
-  };
-  onCollapse = (collapsed) => {
-    console.log(collapsed);
-    this.setState({
-      collapsed: !this.state.collapsed,
-      mode: collapsed ? 'vertical' : 'inline',
-    });
-  }
-  render() {
+	  collapsed: false,
+	  mode: 'inline',
+	}
+	onCollapse = (collapsed) => {
+	  console.log(collapsed);
+	  this.setState({
+	    collapsed: !this.state.collapsed,
+	    mode: collapsed ? 'vertical' : 'inline',
+	  });
+	}
+  render(){
     return (
-      <Layout style={{height: '100%'}}>
-        <Sider
-          collapsible
-          collapsed={this.state.collapsed}
-          onCollapse={this.onCollapse}
-        >
-          <div className="logo" />
+    	<Layout style={{height: '100%'}}>
+    		<Sider
+    		  collapsible
+    		  collapsed={this.state.collapsed}
+    		  onCollapse={this.onCollapse}
+    		>
+    		  <div className="logo" />
 
-          <Menu theme="dark" mode={this.state.mode} defaultSelectedKeys={['/']}>
+    		  <Menu theme="dark" mode={this.state.mode} defaultSelectedKeys={['/']}>
 
-            <Menu.Item key="/"><Link to="">首页</Link></Menu.Item>
-            <SubMenu
-              key="sub1"
-              title={<span><Icon type="user" /><span className="nav-text">用户</span></span>}
-            >
+    		    <Menu.Item key="/"><Link to="/">首页</Link></Menu.Item>
 
-              <Menu.Item key="/userlist"><Link to="/user/userlist">用户列表</Link></Menu.Item>
-              <Menu.Item key="/businesstype"><Link to="/user/businesstype">商家类型</Link></Menu.Item>
-              <Menu.Item key="/businesslist"><Link to="/user/businesslist">商家列表</Link></Menu.Item>
+    		    <SubMenu
+    		      key="/user"
+    		      title={<span><Icon type="user" /><span className="nav-text">用户</span></span>}
+    		    >
+    		      <Menu.Item key="/user/userlist"><Link to="/user/userlist">用户列表</Link></Menu.Item>
+    		      <Menu.Item key="/user/businesstype"><Link to="/user/businesstype">商家类型</Link></Menu.Item>
+    		      <Menu.Item key="/user/businesslist"><Link to="/user/businesslist">商家列表</Link></Menu.Item>
+    		    </SubMenu>
 
-            </SubMenu>
+    		    <SubMenu
+    		      key="/goods"
+    		      title={<span><Icon type="user" /><span className="nav-text">商品</span></span>}
+    		    >
+    		      <Menu.Item key="/goods/goodstype"><Link to="/goods/goodstype">用户列表</Link></Menu.Item>
+    		      <Menu.Item key="/goods/goodsprop"><Link to="/goods/goodsprop">商家类型</Link></Menu.Item>
+    		      <Menu.Item key="/goods/typesku"><Link to="/goods/typesku">商家列表</Link></Menu.Item>
+    		      <Menu.Item key="/goods/goodssku"><Link to="/goods/goodssku">商家列表</Link></Menu.Item>
+    		      <Menu.Item key="/goods/usertype"><Link to="/goods/usertype">商家列表</Link></Menu.Item>
+    		      <Menu.Item key="/goods/usergoods"><Link to="/goods/usergoods">商家列表</Link></Menu.Item>
+    		    </SubMenu>
 
-            <SubMenu
-              key="sub2"
-              title={<span><Icon type="team" /><span className="nav-text">商品</span></span>}
-            >
-              <Menu.Item key="4">Team 1</Menu.Item>
-              <Menu.Item key="5">Team 2</Menu.Item>
-            </SubMenu>
-            <Menu.Item key="6">
-              <span>
-                <Icon type="file" />
-                <span className="nav-text">File</span>
-              </span>
-            </Menu.Item>
-          </Menu>
+    		    <SubMenu
+    		      key="/orders"
+    		      title={<span><Icon type="user" /><span className="nav-text">订单</span></span>}
+    		    >
+    		      <Menu.Item key="/orders/general"><Link to="/orders/general">常规订单</Link></Menu.Item>
+    		      <Menu.Item key="/orders/ious"><Link to="/orders/ious">白条订单</Link></Menu.Item>
+    		      <Menu.Item key="/orders/infor"><Link to="/orders/infor">信息订单</Link></Menu.Item>
+    		      <Menu.Item key="/orders/distribute"><Link to="/orders/distribute">配送订单</Link></Menu.Item>
+    		      <Menu.Item key="/orders/refund"><Link to="/orders/refund">退款订单</Link></Menu.Item>
 
-        </Sider>
-        <Layout>
-          <Header style={{ background: '#fff', padding: 0 }} >
-            <Icon
-              className="trigger"
-              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
-              onClick={this.onCollapse}
-            />
-          </Header>
-          <Content style={{ margin: '0 16px' }}>
-            <Breadcrumb style={{ margin: '12px 0' }}>
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
-            </Breadcrumb>   
-            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-              {this.props.children}
-            </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>
-            Ant Design ©2016 Created by Ant UED
-          </Footer>
-        </Layout>
-      </Layout>
-    );
+    		    </SubMenu>
+
+    		    <SubMenu
+    		      key="/finance"
+    		      title={<span><Icon type="user" /><span className="nav-text">财务</span></span>}
+    		    >
+    		      <Menu.Item key="/finance/account_list"><Link to="/finance/account_list">常规订单</Link></Menu.Item>
+    		      <Menu.Item key="/finance/business_ious"><Link to="/finance/business_ious">白条订单</Link></Menu.Item>
+    		      <Menu.Item key="/finance/withdraw_manage"><Link to="/finance/withdraw_manage">信息订单</Link></Menu.Item>
+    		      <Menu.Item key="/finance/account_manage"><Link to="/finance/account_manage">配送订单</Link></Menu.Item>
+    		      <Menu.Item key="/finance/bill_center"><Link to="/finance/bill_center">退款订单</Link></Menu.Item>
+    		      <Menu.Item key="/finance/bond_manage"><Link to="/finance/bond_manage">退款订单</Link></Menu.Item>
+    		      <Menu.Item key="/finance/bank_card_manage"><Link to="/finance/bank_card_manage">退款订单</Link></Menu.Item>
+
+    		    </SubMenu>
+
+    		    <Menu.Item key="/finance">
+    		    	<Link to="/finance">
+    		    		<Icon type="file" />
+    		        <span className="nav-text">财务</span>
+    		    	</Link>
+    		    </Menu.Item>
+
+    		    <Menu.Item key="/app">
+    		    	<Link to="/app">
+    		    		<Icon type="file" />
+    		        <span className="nav-text">应用</span>
+    		    	</Link>
+    		    </Menu.Item>
+
+    		    <Menu.Item key="/data_analysis">
+    		    	<Link to="/data_analysis">
+    		    		<Icon type="file" />
+    		        <span className="nav-text">数据分析</span>
+    		    	</Link>
+    		    </Menu.Item>
+
+    		    <Menu.Item key="/admin_address">
+    		    	<Link to="/admin_address">
+    		    		<Icon type="file" />
+    		        <span className="nav-text">管理员通讯录</span>
+    		    	</Link>
+    		    </Menu.Item>
+
+    		  </Menu>
+
+    		</Sider>
+
+    		<Layout>
+    		  <Header style={{ background: '#fff', padding: 0 }} >
+    		    <Icon
+    		      className="trigger"
+    		      type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+    		      onClick={this.onCollapse}
+    		    />
+    		  </Header>
+    		  <Content style={{ margin: '0 16px' }}>
+    		    <Breadcrumb separator=">" style={{ margin: '12px 0' }} routes={this.props.routes} params={this.props.params} />
+    		    <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+    		      {this.props.children}
+    		    </div>
+    		  </Content>
+    		  <Footer style={{ textAlign: 'center' }}>
+    		    Ant Design ©2016 Created by Ant UED
+    		  </Footer>
+    		</Layout>
+
+    	</Layout>
+    )
   }
 }
 
-export default App;
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,22 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Router, Route, browserHistory } from 'react-router'
-import App from './App';
+import { Router, Route, browserHistory } from 'react-router';
+
+import App from './App'
+
 import UserList from './routes/user/userlist/';
 import BusinessType from './routes/user/businesstype/';
 import BusinessList from './routes/user/businesslist/';
 
 
-ReactDOM.render((
-	<Router history={browserHistory}>
-	    <Route name="home" breadcrumbName="Home" path="/" component={App}>
-	    	<Route name="user" breadcrumbName="User" path="user">
-			    <Route name="userlist" breadcrumbName="UserList" path="userlist" component={UserList} />
-			    <Route path="businesstype" component={BusinessType} />
-			    <Route path="businesslist" component={BusinessList} />
-	    	</Route>
-	    </Route>
-	  </Router>
-), document.getElementById('root'));
+
+
+
+
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route breadcrumbName="首页" path="/" component={App}>
+      <Route breadcrumbName="用户" path="user">
+				<Route breadcrumbName="用户列表" path="userlist" component={UserList}> </Route>
+				<Route breadcrumbName="商家类型" path="businesstype" component={BusinessType}> </Route>
+				<Route breadcrumbName="商家列表" path="businesslist" component={BusinessList}> </Route>
+      </Route>
+      <Route breadcrumbName="商品" path="goods">
+      	<Route breadcrumbName="商品品类" path="goodstype"> </Route>
+      	<Route breadcrumbName="商品属性" path="goodsprop"> </Route>
+      	<Route breadcrumbName="品种SKU" path="typesku"> </Route>
+      	<Route breadcrumbName="商品SKU" path="goodssku"> </Route>
+      	<Route breadcrumbName="用户品种" path="usertype"> </Route>
+      	<Route breadcrumbName="用户商品" path="usergoods"> </Route>
+      </Route>
+      <Route breadcrumbName="订单" path="orders">
+      	<Route breadcrumbName="常规订单" path="general"> </Route>
+      	<Route breadcrumbName="白条订单" path="ious"> </Route>
+      	<Route breadcrumbName="信息订单" path="infor"> </Route>
+      	<Route breadcrumbName="配送订单" path="distribute"> </Route>
+      	<Route breadcrumbName="退款订单" path="refund"> </Route>
+      </Route>
+      <Route breadcrumbName="财务" path="finance">
+      	<Route breadcrumbName="账户列表" path="account_list"> </Route>
+      	<Route breadcrumbName="商家白条" path="business_ious"> </Route>
+      	<Route breadcrumbName="提现管理" path="withdraw_manage"> </Route>
+      	<Route breadcrumbName="账户管理" path="account_manage"> </Route>
+      	<Route breadcrumbName="对账中心" path="bill_center"> </Route>
+      	<Route breadcrumbName="保证金管理" path="bond_manage"> </Route>
+      	<Route breadcrumbName="银行卡管理" path="bank_card_manage"> </Route>
+      </Route>
+
+      <Route breadcrumbName="应用" path="app"> </Route>
+      <Route breadcrumbName="数据分析" path="data_analysis"> </Route>
+      <Route breadcrumbName="管理员通讯录" path="admin_address"> </Route>
+    </Route>
+  </Router>
+, document.getElementById('root'));
+
+
+
+
 
