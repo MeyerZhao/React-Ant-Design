4d63f2f712a2f151ae3b6b7e4095101c4b703870
Meyerzhao <zhao_min_yuan@126.com>
2017-07-18 17:07:05 +0800

    删除多余的添加属性值  按钮

--- src/routes/goods/goodsprop/index.js
+++ src/routes/goods/goodsprop/index.js
@@ -3,7 +3,6 @@ import { Row, Col, Button } from 'antd';
 import Table from './Table'
 import Modal from '../../../components/Modal.js'
 import Form from './Form'
-import Form2 from './Form2'
 import { Link } from 'react-router';
 
 
@@ -15,7 +14,6 @@ export default class Users extends React.Component {
         <Row style={{marginBottom:'10px'}}>
           <Col span={12}>
             <Modal title="添加属性"> <Form /> </Modal>
-            <Modal title="添加属性值"> <Form2 /> </Modal>
              <Button size="large" type="primary" ><Link to="/goods/goodsprop/goodspropvalue">添加属性值 </Link></Button>
           </Col>
         </Row>
