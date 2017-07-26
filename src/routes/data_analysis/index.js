8a13aa7439afc0834f6baaebee1cfe4225524684
Meyerzhao <zhao_min_yuan@126.com>
2017-07-26 13:40:08 +0800

    一些错误删除

--- src/routes/data_analysis/index.js
+++ src/routes/data_analysis/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 
 export default class index extends React.Component {
-
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div>数据暂无</div>
